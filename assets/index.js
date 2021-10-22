a7b6eda5dceac467e9b559177e3896e66b9cd1d4
carolinarozo <67168080+carolinarozo@users.noreply.github.com>
2021-10-21 23:47:07 -0500

    página dinamica con fechas

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,46 @@
+document.getElementById("adelanteAno").addEventListener("click", function () {
+
+
+    var ano = parseInt(document.getElementById("ano").innerHTML);
+
+
+    var anoSiguiente = ano + 1;
+
+    document.getElementById("ano").innerHTML = anoSiguiente;
+    var mes = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
+    var mesActual = document.getElementById("mes").innerHTML;
+    var indexMesActual = mes.indexOf(mesActual);
+    tablaMes(anoSiguiente, indexMesActual)
+
+
+
+
+})
+
+document.getElementById("atrasAno").addEventListener("click", function () {
+
+
+    var ano = parseInt(document.getElementById("ano").innerHTML);
+
+    var anoSiguiente = ano - 1;
+    document.getElementById("ano").innerHTML = anoSiguiente;
+
+    var mes = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
+    var mesActual = document.getElementById("mes").innerHTML;
+    var indexMesActual = mes.indexOf(mesActual);
+    tablaMes(anoSiguiente, indexMesActual)
+
+
+
+
+})
+
+
 document.getElementById("adelanteMes").addEventListener("click", function () {
     var mes = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
     var mesActual = document.getElementById("mes").innerHTML;
     var indexMesActual = mes.indexOf(mesActual);
+    var ano = parseInt(document.getElementById("ano").innerHTML);
 
     if (indexMesActual < 11) {
 
@@ -10,11 +49,13 @@ document.getElementById("adelanteMes").addEventListener("click", function () {
         var mesSiguiente = mes[indexmesSiguiente];
 
         document.getElementById("mes").innerHTML = mesSiguiente
+        tablaMes(ano, indexmesSiguiente)
 
 
     } else {
 
         document.getElementById("mes").innerHTML = mes[0];
+        tablaMes(ano, 0)
     }
 
 })
@@ -23,6 +64,7 @@ document.getElementById("atrasMes").addEventListener("click", function () {
     var mes = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"];
     var mesActual = document.getElementById("mes").innerHTML;
     var indexMesActual = mes.indexOf(mesActual);
+    var ano = parseInt(document.getElementById("ano").innerHTML);
 
     if (indexMesActual > 0) {
 
@@ -31,11 +73,13 @@ document.getElementById("atrasMes").addEventListener("click", function () {
         var mesSiguiente = mes[indexmesSiguiente];
 
         document.getElementById("mes").innerHTML = mesSiguiente
+        tablaMes(ano, indexmesSiguiente)
 
 
     } else {
 
         document.getElementById("mes").innerHTML = mes[11];
+        tablaMes(ano, 11)
     }
 
 })
@@ -53,8 +97,77 @@ function cargarPagina() {
     document.getElementById("mes").innerHTML = mes;
 
     var anoActual = lista[0];
-    console.log(anoActual)
+
     document.getElementById("ano").innerHTML = anoActual
+    tablaMes(anoActual, nmes - 1)
+
+
+
+
+}
+
+function tablaMes(ano, mes) {
+    var dias = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+    let fecha1 = new Date(ano, mes, 0);
+    let nuDias = fecha1.getDate();
+
+
+    let fecha2 = new Date(ano, mes, 1);
+    let listafecha2 = fecha2.toString().split(" ");
+    console.log(listafecha2)
+
+    let dia = listafecha2[0];
+
+    let indxdia = dias.indexOf(dia)
+
+
+
+
+    var diat = 1
+
+    document.getElementById("tBody").innerHTML = ""
+
+
+    for (let m = 0; m <= 7; m++) {
+
+        var mensaje = ""
+
+
+
+
+        for (let i = 0; i < 7; i++) {
+
+
+            if (diat <= nuDias) {
+
+                if (i < indxdia) {
+                    mensaje += `<td></td>`
+
+
+                } else {
+                    mensaje += `<td>` + diat + `</td>`
+                    diat += 1;
+                    indxdia = 0
+
+
+
+
+
+
+                }
+            }
+        }
+        document.getElementById("tBody").innerHTML += mensaje
+
+
+
+    }
+
+
+
+
+
 
 
 
